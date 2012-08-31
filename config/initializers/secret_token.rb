5a24e8896ab0af644ad562e1a5edcb5937974714
Aaron Crespo <aaroncrespo@gmail.com>
2012-08-30 21:51:31 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DCNeighborhood::Application.config.secret_token = '4cec5f0c55fa91aa1256ea0c4ac92175a4c6d3d1da96c3321f7b22e67c69fd12ea7749c40b380d6e96caa4ce741f8bcca626c38f94819ac211c9d31f3db12ef0'

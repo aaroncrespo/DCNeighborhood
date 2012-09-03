d6c3b79b899ec96593e2a6156fa8a0ffc10d6d97
Aaron Crespo <aaroncrespo@gmail.com>
2012-09-03 17:43:02 -0400

    cancan and rolify working together to limit access to address editing currently all users are admins

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,11 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+
     if user.has_role? :admin
-      can :manage, :all
+        can :manage, :all
+    else
+        can :read, Address
     end
     # Define abilities for the passed in user here. For example:
     #
